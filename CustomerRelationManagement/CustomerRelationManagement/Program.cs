070e6fb15e27ab450729429013f34b3961bd99b3
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-09-23 10:16:06 +0200

    created class for customer info

---
+++ CustomerRelationManagement/CustomerRelationManagement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRelationManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lista med kundernas info 
+            System.Collections.ArrayList Customers = new System.Collections.ArrayList();
+
+            //fråga om de är registrerade
+            //om inte mata in kundens information (namn, telefon nummer, email, favortier, 
+
+            Customers.Add(Console.ReadLine());
+
+
+            //skriv ut deras information, antalet favoriter
+        }
+    }
+}
